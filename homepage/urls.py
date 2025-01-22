d9d60828d4a4f380dc29caef9fe23390a17dc450
Mikhail <57218551+XimoNeem@users.noreply.github.com>
2025-01-22 19:01:43 +0300

    basic-pages-design | for-debug

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),  # главная страница
+    path('artist/', views.artist_page, name='artist'),  # страница /artist
 ]
